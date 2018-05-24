f97030037ee30c9ab340ef008c6463351caa9192
ThomasEcalle <thomasecalle@hotmail.fr>
2018-05-24 20:16:24 +0200

    refactored and added notification on payment

--- index.js
+++ index.js
@@ -10,17 +10,49 @@ exports.validPayment = functions.https.onRequest((req, res) => {
 
     const identifier = req.query.identifier;
     const amount = req.query.amount;
-    const payer = req.query.payer;
+    const payerId = req.query.payerId;
+    const payerName = req.query.payerName;
+    const receiverId = req.query.receiverId;
+    const receiverName = req.query.receiverName;
 
     const payment = {
         success: true,
         date: new Date().getTime(),
         amount: amount,
-        payer: payer,
+        payerId: payerId,
+        payerName: payerName,
+        receiverId: receiverId,
+        receiverName: receiverName,
     };
 
-    return admin.database().ref('/payments/${identifier}').push(payment).then((snapshot) => {
-    return res.redirect(303, snapshot.ref.toString());
+    return admin.database().ref('/payments').child(identifier).push(payment).then((snapshot) => {
+
+        return admin.database().ref("/users/" + receiverId).once('value').then(snap => {
+            const token = snap.child("messaging_token").val();
+            console.log("token: ", token);
+
+            //we have everything we need
+            //Build the message payload and send the message
+            console.log("Construction the notification message.");
+            const payload = {
+                data: {
+                    data_type: "direct_message",
+                    title: "Vous avez reçu un nouveau virement !" ,
+                    message: payerName + " vous a fait un virement de " + amount,
+                    message_id: payerId + receiverId,
+                }
+            };
+
+            return admin.messaging().sendToDevice(token, payload)
+                .then((response) => {
+                    console.log("Successfully sent message:", response);
+                    return res.redirect(303, snapshot.ref.toString());
+                })
+                .catch((error) => {
+                    console.log("Error sending message:", error);
+                    return res.redirect(303, snapshot.ref.toString());
+                });
+        });
     });
 
 });
@@ -28,18 +60,79 @@ exports.validPayment = functions.https.onRequest((req, res) => {
 exports.unValidPayment = functions.https.onRequest((req, res) => {
 
     const identifier = req.query.identifier;
+
     const amount = req.query.amount;
-    const payer = req.query.payer;
+    const payerId = req.query.payerId;
+    const payerName = req.query.payerName;
+    const receiverId = req.query.receiverId;
+    const receiverName = req.query.receiverName;
 
     const payment = {
         success: false,
         date: new Date().getTime(),
         amount: amount,
-        payer: payer,
+        payerId: payerId,
+        payerName: payerName,
+        receiverId: receiverId,
+        receiverName: receiverName,
     };
 
-    return admin.database().ref('/payments/${identifier}').push(payment).then((snapshot) => {
+    return admin.database().ref('/payments').child(identifier).push(payment).then((snapshot) => {
         return res.redirect(303, snapshot.ref.toString());
     });
 
+});
+
+exports.sendNotification = functions.database.ref('/notifications/{userId}/{messageId}').onWrite(event => {
+
+    /*
+    //get the userId of the person receiving the notification because we need to get their token
+    const receiverId = event.params.userId;
+    console.log("receiverId: ", receiverId);
+
+    //get the user id of the person who sent the message
+    const senderId = event.data.child('user_id').val();
+    console.log("senderId: ", senderId);
+
+    //get the message
+    const message = event.data.child('message').val();
+    console.log("message: ", message);
+
+    //get the message id. We'll be sending this in the payload
+    const messageId = event.params.messageId;
+    console.log("messageId: ", messageId);
+
+    //query the users node and get the name of the user who sent the message
+    return admin.database().ref("/users/" + senderId).once('value').then(snap => {
+        const senderName = snap.child("name").val();
+        console.log("senderName: ", senderName);
+
+    //get the token of the user receiving the message
+    return admin.database().ref("/users/" + receiverId).once('value').then(snap => {
+        const token = snap.child("messaging_token").val();
+    console.log("token: ", token);
+
+    //we have everything we need
+    //Build the message payload and send the message
+    console.log("Construction the notification message.");
+    const payload = {
+        data: {
+            data_type: "direct_message",
+            title: "New Message from " + senderName,
+            message: message,
+            message_id: messageId,
+        }
+    };
+
+    return admin.messaging().sendToDevice(token, payload)
+        .then(function(response) {
+            console.log("Successfully sent message:", response);
+        })
+        .catch(function(error) {
+            console.log("Error sending message:", error);
+        });
+                });
+            });
+
+            */
 });
